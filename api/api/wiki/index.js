97d48d03648c6639f923d58edf998ac13f908176
huan@xuanmbp <hhhust@qq.com>
2016-02-24 23:53:55 +0800

    cache the data into db

--- api/api/wiki/index.js
+++ api/api/wiki/index.js
@@ -3,6 +3,12 @@ var fetch = require('isomorphic-fetch');
 var $ = require('cheerio');
 var debug = require('debug')('api');
 var parse = require('co-body');
+var redis = require('redis');
+
+var db = redis.createClient()
+db.on('ready',function(){
+  console.log('redis connected!')
+})
 
 function definitions(root,type){
 	var typeElements = $("#" + type,root);
@@ -22,37 +28,65 @@ exports.all = function *() {
   	 var body = yield parse(this);
   	 word = body.text
   }
-  var baseURL = 'http://en.wiktionary.org';
-  var url = baseURL + '/w/api.php?action=parse&format=json&prop=text|revid|displaytitle&page=' + word;
   var text = ""
-  var now = new Date().getTime()
-  yield fetch(url).then((resp) =>{
-  		console.log("crawl " + word + " from wiktionary.org done, time used " + (new Date().getTime() - now) + "ms")	
+  var cache;
+  yield new Promise((resolve,reject) => {
+    db.hget("wiki",word,(err,v) => {
+      if(err){
+        reject(err)
+      }else {
+        resolve(v)
+      }
+    })
+  }).then((v) => {
+        console.log("get cache for word %s",word)
+      cache = v;
+    }).catch((err) => {
+      console.log(err)
+    })
+    // .then(console.log)
+    // .catch(console.log)
+  // })
+  if(cache){
+    text = cache;
+    console.log("fetch from cache, word=%s",word)
+  }
+  else {
+    var baseURL = 'http://en.wiktionary.org';
+    var url = baseURL + '/w/api.php?action=parse&format=json&prop=text|revid|displaytitle&page=' + word;
+    var now = new Date().getTime()
+    yield fetch(url).then((resp) =>{
+      console.log("crawl " + word + " from wiktionary.org done, time used " + (new Date().getTime() - now) + "ms")  
         return resp.json();
       }).then((json) => {
         if(json.parse){
-			var html = json.parse.text['*'];
-			var root = $("#English",$(html)).parent().nextAll();//直接抓取english节点
-			var types = ["Noun","Verb","Pronoun","Interjection"];
-			var all = []
-			types.forEach(function(type){
-				var defs = definitions(root,type);
-			    all = all.concat(defs)
-			});
-			return all
+          var html = json.parse.text['*'];
+          var root = $("#English",$(html)).parent().nextAll();//直接抓取english节点
+          var types = ["Noun","Verb","Pronoun","Interjection"];
+          var all = []
+          types.forEach(function(type){
+            var defs = definitions(root,type);
+              all = all.concat(defs)
+          });
+          return all
         }
       },
       (e) => {
-      	console.log('word ' + word + " fail to parse the data");
+        console.log('word ' + word + " fail to parse the data");
       })
       .then((all) => {
-      	all.map((a) => {text += a})
+        all.map((a) => {text += a})
       }, (e) => {});
+
+      db.hset("wiki",word,text)
+      console.log("cache word %s into cache",word)
+  }
+  
   if(text.length == 0) {
 	  this.status = 404;
   }else{
 	  this.body = JSON.stringify({response_type:'in_channel',text:text});
-          this.type = "application/json"
+    this.type = "application/json"
 	  this.status = 200;
   }
 }
