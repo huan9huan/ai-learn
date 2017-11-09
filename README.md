444ce818d46ae1a0907b7865ffa8d948fabd05eb
huan9huan <hhhust@gmail.com>
2017-11-09 13:15:55 +0800

    basic nn practise

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 todo: learn from tensorflow 101
 next level: learn from effective tf
+
+## how to run notebooks
+```
+jupyter notebook --ip 127.0.0.1 --notebook-dir ./notebooks --config ~/jupyter_notebook_config.py 
+```
+
+## in local run jupyter
+
+```
+cd .
+jupyter notebook
+```
--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,6 +4,8 @@ server=l2
 pushd .
 cd `dirname $0`
 echo "scp nginx conf to l2 ..."
+scp ./jupyter_notebook_config.py ${server}:~/
+scp ./j.deepword.co.password ${server}:/etc/nginx/conf.d/
 scp ./j.deepword.conf ${server}:/etc/nginx/conf.d/
 echo "reload the nginx..."
 ssh ${server} "/etc/init.d/nginx reload"
