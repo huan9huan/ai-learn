5462cdab1cf71b08d1977469de96e49460be6700
huan9huan <hhhust@gmail.com>
2018-07-30 22:55:13 +0800

    learing

---
+++ yolo/test.py
@@ -0,0 +1,9 @@
+from keras.preprocessing.image import ImageDataGenerator, load_img, img_to_array
+
+train_datagen = ImageDataGenerator(
+        rescale=1./255,
+        shear_range=0.2,
+        zoom_range=0.2,
+        horizontal_flip=True)
+
+test_datagen = ImageDataGenerator(rescale=1./255)
